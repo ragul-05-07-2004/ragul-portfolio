8a64534f3e8d00630bd6775b9a07bed354749714
Ragul R <ragulrajagopal2004@gmail.com>
2026-03-11 17:13:21 +0530

    fix gsap import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Navbar.css";
 
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
-export let smoother: ScrollSmoother;
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);export let smoother: ScrollSmoother;
 
 const Navbar = () => {
   useEffect(() => {
